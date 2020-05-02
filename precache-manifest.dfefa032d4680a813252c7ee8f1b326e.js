73f8e8c28d2988941f89990a5ac84ab33eea7688
Addy2012 <kumaranand287@gmail.com>
2020-05-03 01:50:58 +0530

    Updates

---
+++ precache-manifest.dfefa032d4680a813252c7ee8f1b326e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c784c9573678743d9464b75f5300b5b",
+    "url": "/coronaTracker/index.html"
+  },
+  {
+    "revision": "1c6825f4e894eacdd872",
+    "url": "/coronaTracker/static/css/main.fdb1aad6.chunk.css"
+  },
+  {
+    "revision": "7adece99f929115734e0",
+    "url": "/coronaTracker/static/js/2.ceefc7c0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronaTracker/static/js/2.ceefc7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6825f4e894eacdd872",
+    "url": "/coronaTracker/static/js/main.e3d3c22e.chunk.js"
+  },
+  {
+    "revision": "319d51be0094bd6a4e99",
+    "url": "/coronaTracker/static/js/runtime-main.8280af46.js"
+  }
+]);
